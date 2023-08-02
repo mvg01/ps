ca652d0d5a1854305981cb8968cc09317cd76732
MoongTeng <dkms625@naver.com>
2023-08-02 23:52:10 +0900

    9996 한국이 그리울 땐 서버에 접속하지

---
+++ 알고리즘문제/09996(문자열).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, star;
+	string p;
+	cin >> n;
+	cin >> p;
+	string front, back;
+	for (int i = 0; ; i++) {
+		if (p.at(i) != '*')
+			front.push_back(p.at(i));
+		else {
+			star = i;
+			break;
+		}
+	}
+	for (int i = star + 1; i < p.size(); i++)
+		back.push_back(p.at(i));
+
+	while (n--) {
+		string t;
+		cin >> t;
+
+		bool chk = 1;
+		int tsize = t.size();
+		if (tsize < p.size()-1) {  //예외처리
+			cout << "NE" << '\n';
+			continue;
+		}
+
+		for (int i = 0; i < front.size(); i++) {
+			if (front.at(i) != t.at(i)) {
+				chk = false;
+				break;
+			}
+		}
+		int idx = 0;
+		for (int i = tsize - back.size(); i < tsize; i++) {
+			if (back.at(idx++) != t.at(i)) {
+				chk = false;
+				break;
+			}
+		}
+		if (chk)
+			cout << "DA" << '\n';
+		else
+			cout << "NE" << '\n';
+	}
+}
