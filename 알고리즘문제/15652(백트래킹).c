a9651aeaed50741eb9167cdef0ab638f0252272e
MoongTeng <dkms625@naver.com>
2023-08-14 14:25:56 +0900

    15652 N과 M (4)

---
+++ 알고리즘문제/15652(백트래킹).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int n, m;
+int num[9] = { 0 };
+int res[9] = { 0 };
+int idx = 0;
+void bt(int a, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++)
+			printf("%d ", res[i]);
+		
+		printf("\n");
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!num[i] || i == a) {
+			if (i >= a) {
+				res[cnt] = i;
+				num[i] = 1;
+				bt(i, cnt + 1);
+				num[i] = 0;
+			}
+		}
+	}
+}
+
+int main() 
+{
+	scanf("%d %d", &n, &m);
+	bt(1, 0);
+}
