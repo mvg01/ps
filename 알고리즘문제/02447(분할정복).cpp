ebba1476074d491a33be4761e5430806ce9636a9
MoongTeng <dkms625@naver.com>
2023-08-23 13:46:27 +0900

    02447 별 찍기 - 10

--- 알고리즘문제/02447(분할정복).cpp
+++ 알고리즘문제/02447(분할정복).cpp
@@ -1,59 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-typedef struct {
-	int top;
-	int* data;
-}Stack;
-void init(Stack* s, int n) {
-	s->data = (int*)malloc(sizeof(int) * n);
-	s->top = -1;
-}
-void push(Stack* s, int d) {
-	s->data[++s->top] = d;
-}
-void pop(Stack* s) {
-	s->top--;
-}
-int top(Stack* s) {
-	if (s->top == -1)
-		return -1;
-	return s->data[s->top];
+char mat[2188][2188];
+
+void f(int len, int y, int x) {
+	if (len == 3) {
+		for (int i = y; i < y + 3; i++) {
+			for (int j = x; j < x + 3; j++) {
+				if (!(i == y + 1 && j == x + 1))
+					mat[i][j] = '*';
+			}
+		}
+		return;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (!(i == 1 && j == 1))
+				f(len / 3, y + len / 3 * i, x + len / 3 * j);
+		}
+	}
+
 }
 
 int main()
 {
-	int n, p, num, fret, res = 0;
-	Stack s[6];   //기타줄 개수 만큼 스택이 필요
-	scanf("%d %d", &n, &p);
-	for (int i = 0; i < 6; i++)
-		init(&s[i], p);  //각 스택에 최대 p개 까지 담을 수 있어야 함
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			mat[i][j] = ' ';
+	}
+	f(n, 0, 0);
 
 	for (int i = 0; i < n; i++) {
-		scanf("%d %d", &num, &fret);
-		if (top(&s[num - 1]) < fret) {
-			push(&s[num - 1], fret);
-			res++;
-		}
-		else if (top(&s[num - 1]) > fret) {
-			while (1) {
-				if (top(&s[num - 1]) <= fret || s[num - 1].top == -1)
-					break;
-				else
-					pop(&s[num - 1]);
-
-				res++;
-			}
-
-			if (top(&s[num - 1]) != fret) {  //이미 프렛위에 손이 있다면 손가락을 움직이지 않고 넘어감
-				push(&s[num - 1], fret);
-				res++;
-			}
-		}
-		else  //이미 프렛위에 손이 있다면 손가락을 움직이지 않고 넘어감
-			continue;
+		for (int j = 0; j < n; j++)
+			cout << mat[i][j];
+		cout << '\n';
 	}
-	printf("%d", res);
-	for (int i = 0; i < 6; i++)
-		free(s[i].data);
 }
