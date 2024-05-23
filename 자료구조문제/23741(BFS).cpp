de1929600e19031bca49487dd55dbb88e9f51c55
Donggeon Lee <dkms625@naver.com>
2024-05-24 01:26:44 +0900

    23741 야바위 게임

--- 자료구조문제/23741(BFS).cpp
+++ 자료구조문제/23741(BFS).cpp
@@ -49,3 +49,51 @@ int main() {
     input();
     solve();
 }
+
+
+// dp 활용 풀이 bfs와 크게 다를 건 없다..
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, x, y;
+vector<int>mat[1001];
+bool visited[1001][1001];
+
+void input() {
+    int a, b;
+    cin >> n >> m >> x >> y;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        mat[a].push_back(b);
+        mat[b].push_back(a);
+    }
+}
+
+void f(int a, int b) {
+    for (int i = 0; i < mat[b].size(); i++) 
+        visited[a][mat[b][i]] = 1;
+}
+
+void solve() {
+    if (mat[x].empty()) {
+        cout << -1;
+        return;
+    }
+    visited[0][x] = 1;
+    for (int i = 1; i <= y; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (visited[i - 1][j])
+                f(i, j);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+        if (visited[y][i]) cout << i << ' ';
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    input();
+    solve();
+}
