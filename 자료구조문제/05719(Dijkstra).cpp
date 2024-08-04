21b803bbe75dfefabd2a28e5434f1e537ba76f55
Donggeon Lee <dkms625@naver.com>
2024-08-04 19:58:02 +0900

    05719 거의 최단 경로

---
+++ 자료구조문제/05719(Dijkstra).cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#define INF 999999999
+using namespace std;
+
+int n, m, s, d;
+int dis[501];
+bool visited[501][501];  // 최단경로의 정보 저장
+vector<pair<int, int>>v[501];
+vector<int>temp[501];  // 최단경로의 정보 저장 
+
+void dijkstra() {
+    for (int i = 0; i <= n; i++) dis[i] = INF;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({ 0,s });
+    dis[s] = 0;
+    while (!pq.empty()) {
+        int dist = pq.top().first;
+        int ver = pq.top().second;
+        pq.pop();
+        for (int i = 0; i < v[ver].size(); i++) {
+            int next_ver = v[ver][i].first;
+            int w = v[ver][i].second;
+            if (visited[next_ver][ver])
+                continue;
+            if (dis[next_ver] > dist + w) {
+                dis[next_ver] = dist + w;
+                temp[next_ver].clear();
+                temp[next_ver].push_back(ver);
+                pq.push({ dist + w,next_ver });
+            }
+            else if (dis[next_ver] == dist + w) {
+                temp[next_ver].push_back(ver);
+            }
+        }
+    }
+}
+
+
+void remove_shortest_path() {
+    queue<int>q;
+    q.push(d);
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        for (int i = 0; i < temp[x].size(); i++) {
+            if (!visited[x][temp[x][i]]) {
+                visited[x][temp[x][i]] = 1;
+                q.push(temp[x][i]);
+            }
+        }
+    }
+}
+
+void solve() {
+    dijkstra();
+    remove_shortest_path();
+    dijkstra();
+    if (dis[d] != INF)
+        cout << dis[d] << '\n';
+    else
+        cout << -1 << '\n';
+}
+
+void input() {
+    while (1) {
+        cin >> n >> m;
+        if (n == 0 && m == 0) break;
+        cin >> s >> d;
+        for (int i = 0; i < m; i++) {
+            int a, b, c;
+            cin >> a >> b >> c;
+            v[a].push_back({ b,c });
+        }
+        solve();
+        for (int i = 0; i <= n; i++) {
+            v[i].clear();
+            temp[i].clear();
+            for (int j = 0; j <= n; j++)
+                visited[i][j] = 0;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    input();
+}
